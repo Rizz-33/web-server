cec978cd6f4c6039ef0330e46ee345d85182eaab
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-14 12:01:45 +0530

    feat: Form handler

--- main.go
+++ main.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm error: %v", err)
+        return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %v", name)
+	fmt.Fprintf(w, "Address: %v", address)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
