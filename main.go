d7f85339240ccf59893f937cec7a9d330be18d7b
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-14 11:55:57 +0530

    feat: Hello handler

--- main.go
+++ main.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+	fmt.Fprintf(w, "Hello")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
