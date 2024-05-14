2f4737bd4e771c17cd42dc4be16a43156cd07c79
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-14 11:41:25 +0530

    feat: Main func

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
